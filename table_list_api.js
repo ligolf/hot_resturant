9b3e5f7bbde58b5bd9e3efede352ef84239fb9cc
gilllian ginsberg <gillian.ginsberg@gmail.com>
2019-06-12 21:47:12 -0700

    changed to port 3030

--- table_list_api.js
+++ table_list_api.js
@@ -23,7 +23,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = 3030;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({
@@ -92,7 +92,7 @@ app.post("/api/tables", function (req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3030;
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
